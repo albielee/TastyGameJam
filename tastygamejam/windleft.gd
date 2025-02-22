53907db2ff9ccdc62f8ac4c814b97c00e2e3f1ad
albielee <albertoarjanlee@gmail.com>
2025-02-22 18:50:11 +0000

    wind stuff

--- tastygamejam/windleft.gd
+++ tastygamejam/windleft.gd
@@ -4,7 +4,7 @@ extends Area2D
 @onready var collision_shape = $CollisionShape2D
 @onready var gpu_particles = $GPUParticles2D
 @onready var timer = $Timer
-var facing_direction = -3.14159
+var facing_direction = 3.14159
 
 @export var TIMED_WIND = false
 @export var WIND_INTERNAL = 0
@@ -16,7 +16,6 @@ func _ready() -> void:
 	var material = gpu_particles.process_material
 	material.emission_box_extents = Vector3(collision_shape.shape.size.x/2 + 200,collision_shape.shape.size.y/2,0)
 	gpu_particles.amount = (collision_shape.shape.size.x/collision_shape.shape.size.y) * 10
-
 
 func _on_timer_timeout() -> void:
 	if(TIMED_WIND):
