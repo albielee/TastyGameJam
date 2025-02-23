e64d5801c6f8a0aab9f62047feb856a4a4dce570
sammo4gun <33280495+sammo4gun@users.noreply.github.com>
2025-02-23 12:15:42 +0000

    loading menu changes

--- tastygamejam/endscreen.gd
+++ tastygamejam/endscreen.gd
@@ -11,23 +11,17 @@ var t = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_anything_pressed():
-		start_game = true
 	if start_game and t < time_to_fade_out+wait_after_fade:
-		if t <= time_to_fade_out:
+		if t < time_to_fade_out:
 			modulator.color = Color(
 				1-(1*(t/time_to_fade_out)),
 				1-(1*(t/time_to_fade_out)),
 				1-(1*(t/time_to_fade_out)),
-				1
-			)
-		else:
-			modulator.color = Color(
-				0,
-				0,
-				0,
 				1
 			)
 		t += delta
 		if t >=  time_to_fade_out+wait_after_fade:
 			get_tree().change_scene_to_file("res://game.tscn")
+
+func _on_button_pressed() -> void:
+	start_game = true
